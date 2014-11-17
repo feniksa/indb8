3bfd41cbb9cb3c83717c06fa6662d68e6c54e538
Nikolay Orliuk <nikolay.orliuk@lge.com>
2014-11-17 23:04:00 +0200

    add MojEasySignal/Slot
    - MojEasySignal and slot added to use without handle/signaler
      time-life prolongation
    - simple test in gtest_mojocore
    
    Open-webOS-DCO-1.0-Signed-off-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    
    Change-Id: Ia9110471dd91f8bda6269251c214ce83d01f0c2f
    Reviewed-on: https://g2g.lgsvl.com/6956
    Reviewed-by: DCO Verification
    Reviewed-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Tested-by: Nikolay Orliuk <nikolay.orliuk@lge.com>
    Reviewed-by: Maksym Sditanov <maxim.sditanov@lge.com>

--- inc/core/MojSignal.h
+++ inc/core/MojSignal.h
@@ -1,6 +1,6 @@
 /* @@@LICENSE
 *
-*      Copyright (c) 2009-2013 LG Electronics, Inc.
+*      Copyright (c) 2009-2014 LG Electronics, Inc.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.
@@ -19,6 +19,8 @@
 
 #ifndef MOJSIGNAL_H_
 #define MOJSIGNAL_H_
+
+#include <functional>
 
 #include "core/MojCoreDefs.h"
 #include "core/MojList.h"
@@ -279,5 +281,75 @@ public:
 };
 
 #include "core/internal/MojSignalInternal.h"
+
+template <typename... Args>
+class MojEasySlot
+{
+    struct Handler : MojSignalHandler
+    {
+        typedef typename MojSignal<Args...>::template Slot<Handler> SlotType;
+        SlotType slot;
+
+        std::function<MojErr(Args...)> handler;
+
+        MojErr handle(Args... args) { return handler(args...); }
+
+        Handler(decltype(handler) func) :
+            slot(this, &Handler::handle),
+            handler(std::move(func))
+        {}
+    };
+
+    // handler always should be ref-counted
+    MojRefCountedPtr<Handler> m_handler;
+public:
+
+    MojEasySlot(std::function<MojErr(Args...)> handler) :
+        m_handler(new Handler(handler))
+    {}
+
+    ~MojEasySlot() { cancel(); }
+
+    void cancel() { m_handler->slot.cancel(); }
+
+    typename Handler::SlotType &slot() { return m_handler->slot; }
+
+};
+
+template <class... Args>
+class MojEasySignal
+{
+    struct Handler : MojSignalHandler
+    {
+        typedef MojSignal<Args...> SignalType;
+        SignalType signal;
+
+        Handler() : signal(this) {}
+    };
+
+    // handler always should be ref-counted
+    MojRefCountedPtr<Handler> m_handler;
+public:
+    template <typename T>
+    using Slot = typename Handler::SignalType::template Slot<T>;
+
+    typedef MojEasySlot<Args...> EasySlot;
+
+    MojEasySignal() : m_handler(new Handler()) {}
+
+    typename Handler::SignalType &signal() { return m_handler->signal; }
+
+    void connect(typename Handler::SignalType::SlotRef ref)
+    { m_handler->signal.connect(ref); }
+
+    void connect(EasySlot &easySlot)
+    { m_handler->signal.connect(easySlot.slot()); }
+
+    MojErr call(Args... args)
+    { return m_handler->signal.call(args...); }
+
+    MojErr fire(Args... args)
+    { return m_handler->signal.fire(args...); }
+};
 
 #endif /* MOJSIGNAL_H_ */
